c6a5497b7d86f616ac8841f227044b33c4fa4696
Shane Boner <csgomakesmemad@gmail.com>
2015-06-22 19:16:20 -0600

    Turned config into a module

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-var toml = require('toml'),
-    fs   = require('fs');
-
-// load the configuration file
-fs.readFile("config.toml", function(err, config) {
-    if (err) {
-        console.log("Error reading configuration file: " + err);
-    } else {
-        config = toml.parse(config);
-
-        console.log(config);
-    }
+require('./config').init(function(config) {
+    console.log(config.get("bot.name"));
 })
---
+++ config.js
@@ -0,0 +1,34 @@
+var fs   = require('fs'),
+    toml = require('toml');
+
+// load the configuration file
+exports.init = function(cb) {
+    fs.readFile("config.toml", function(err, config) {
+        if (err) {
+            console.log("Error reading configuration file: " + err);
+        } else {
+            config = toml.parse(config);
+
+            var new_config = {
+                get: function(field) {
+                    var obj = config;
+                    var prop, props = field.split('.');
+
+                    for (var i = 0, iLen = props.length - 1; i < iLen; i++) {
+                        prop = props[i];
+
+                        var candidate = obj[prop];
+                        if (candidate !== undefined) {
+                            obj = candidate;
+                        } else {
+                            break;
+                        }
+                    }
+                    return obj[props[i]];
+                }
+            };
+
+            cb(new_config);
+        }
+    })
+}
