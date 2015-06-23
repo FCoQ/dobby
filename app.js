3e04a39ece97eee4288f72624c95af3f1e2f92ff
Shane Boner <csgomakesmemad@gmail.com>
2015-06-22 18:27:40 -0600

    Read a configuration file and print its contents

---
+++ app.js
@@ -0,0 +1,13 @@
+var toml = require('toml'),
+	fs   = require('fs');
+
+// load the configuration file
+fs.readFile("config.toml", function(err, config) {
+	if (err) {
+		console.log("Error reading configuration file: " + err);
+	} else {
+		config = toml.parse(config);
+
+		console.log(config);
+	}
+})
