80da6e5dd9ca872d49be3e11bdf633f1b0a94dba
Shane Boner <csgomakesmemad@gmail.com>
2015-06-22 18:29:04 -0600

    Fix indentation

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var toml = require('toml'),
-	fs   = require('fs');
+    fs   = require('fs');
 
 // load the configuration file
 fs.readFile("config.toml", function(err, config) {
-	if (err) {
-		console.log("Error reading configuration file: " + err);
-	} else {
-		config = toml.parse(config);
+    if (err) {
+        console.log("Error reading configuration file: " + err);
+    } else {
+        config = toml.parse(config);
 
-		console.log(config);
-	}
+        console.log(config);
+    }
 })
