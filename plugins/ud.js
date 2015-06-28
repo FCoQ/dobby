4bf715e8d8b2f37985122773b37e03680faa74a1
Quib Daddy <webmaster@quibs.org>
2015-06-28 16:15:12 -0700

    urban dictionary plugin added

--- plugins/ud.js
+++ plugins/ud.js
@@ -1,5 +1,28 @@
 // urban dictionary plugin for Dobby
 
+var unirest = require('unirest');
+
+
 exports.onMessage = function(msg, dobby) {
-    dobby.respond("Hey.");
+    var terms = msg.split(" ");
+    var command = terms.shift();
+    terms = terms.join(" ");
+
+    if (command == '.ud') {
+        unirest.get("http://api.urbandictionary.com/v0/define")
+            .query({term: terms})
+            .end(function(response) {
+                if (response.status == 200) {
+                    var first = response.body.list[0];
+                    if (typeof first != "undefined") {
+                        dobby.respond("[B]Urban Dictionary:[/B] " + first.definition);
+                    } else {
+                        dobby.respond("No results.");
+                    }
+                } else {
+                    dobby.respond("Failed!");
+                }
+            })
+    }
 }
+
