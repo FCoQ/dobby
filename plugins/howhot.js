17ed79d1931a25bb99dd5eef4eccf92ab100c3c5
Quib Daddy <webmaster@quibs.org>
2015-06-30 18:58:40 -0700

    Finished .howhot, tim sucks

--- plugins/howhot.js
+++ plugins/howhot.js
@@ -15,26 +15,45 @@ exports.config = function(config){
 exports.onMessage = function(msg, dobby) {
     var terms = msg.split(" ");
     var command = terms.shift();
-    //terms = terms.join(" ");
+    terms = terms.join(" ");
 
     if (command == '.howhot') {
         async.waterfall([
            function(cb){
-               dobby.client_from().get_ip(function(err, ip){
-                   if(!err){
-                       unirest.get("http://api.ipinfodb.com/v3/ip-city/")
-                           .query({key: key, ip: ip, format: "json"})
-                           .end(function(response) {
-                               if (response.status == 200) {
-                                   cb(null, {zip: response.body.zipCode, country: response.body.countryCode})
-                               } else {
-                                   cb("Couldn't find ip!");
-                               }
-                           })
-                   } else {
-                       cb(err)
-                   }
-               })
+               if(terms.length > 0){
+                   dobby.find_clients(terms, function(err, clients){
+                       if(err){
+                           cb(err)
+                       }else{
+                           if(clients.length == 1){
+                               cb(null, clients[0])
+                           }else if(clients.length == 0){
+                               dobby.respond("Couldn't find that user!")
+                           }else{
+                               dobby.respond("More than one user fits this criteria!")
+                           }
+                       }
+                   })
+               }else{
+                   cb(null, dobby.client_from())
+               }
+           },
+           function(client, cb){
+             client.get_ip( function(err, ip){
+                 if(!err){
+                     unirest.get("http://api.ipinfodb.com/v3/ip-city/")
+                         .query({key: key, ip: ip, format: "json"})
+                         .end(function(response) {
+                             if (response.status == 200) {
+                                 cb(null, {zip: response.body.zipCode, country: response.body.countryCode})
+                             } else {
+                                 cb("Couldn't find ip!");
+                             }
+                         })
+                 } else {
+                     cb(err)
+                 }
+             })
            },
            function(location, cb){
                unirest.get("http://api.openweathermap.org/data/2.5/weather")
