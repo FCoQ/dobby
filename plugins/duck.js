957967e9378061bec40a436c411b663c90ab41bd
Quib Daddy <webmaster@quibs.org>
2015-07-11 17:16:00 -0700

    Added .g to google.js plugin, fixed bugs in duck.js

--- plugins/duck.js
+++ plugins/duck.js
@@ -7,32 +7,33 @@ exports.onMessage = function(msg, dobby) {
     var command = terms.shift();
     terms = terms.join(" ");
 
-    if (command == "!"){
+    if (command == "!") {
         dobby.respond("[B]Example:[/B] !amazon butt plugs. List of !bangs found [URL=https://duckduckgo.com/bang#bangs-list]here[/URL].");
-
-        } else {
-            command[0] == "!";
-            {
-            unirest.get("http://api.duckduckgo.com/")
-                .query({q: command + " " + terms, format: "json",no_html: 1, no_redirect: 1})
-                .end(function(response) {
-                    if (response.status == 200) {
-                        var body = JSON.parse(response.body);
-                        if (body){
-                            var first = body;
+    } else if (command[0] == "!") {
+        unirest.get("http://api.duckduckgo.com/")
+            .query({q: command + " " + terms, format: "json",no_html: 1, no_redirect: 1})
+            .end(function(response) {
+                if (response.status == 200) {
+                    var body = JSON.parse(response.body);
+                    if (body){
+                        var first = body;
+                        if (!first.Redirect){
+                            dobby.respond("[B]Example:[/B] !amazon butt plugs. List of !bangs found [URL=https://duckduckgo.com/bang#bangs-list]here[/URL].");
+                        } else {
                             if (typeof first != "undefined") {
                                 dobby.respond("[URL]" + first.Redirect + "[/URL]");
                             } else {
                                 dobby.respond("No results.");
                             }
-                        } else {
-                            dobby.respond("No response.");
                         }
                     } else {
-                        dobby.respond("Failed!");
+                        dobby.respond("No response.");
                     }
-                })
-        }
+                } else {
+                    dobby.respond("Failed!");
+                }
+            })
     }
 }
+
 
