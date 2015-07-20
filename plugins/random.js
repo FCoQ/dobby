8172887a2cd3f6c59da9d20df61cd652de22466c
Quib Daddy <webmaster@quibs.org>
2015-07-19 18:45:48 -0700

    Added error to .random + added .random entry to config.toml.example

--- plugins/random.js
+++ plugins/random.js
@@ -1,15 +1,18 @@
 // random integer plugin
 
 exports.help = [
-    [".random <min>-<max>", "Generate random integer between an interval"]
+    [".random <min>-<max>","Generate random integer between an xxxxxx-yyyyyy"]
 ]
 
 exports.onMessage = function(msg, dobby) {
-    var s = /^\.rand(om)? ([0-9]{1,6})-([0-9]{1,6})/.exec(msg);
+    var s = /^\.rand(om)? ([0-9]{1,6})-([0-9]{1,6})$/.exec(msg);
+    var almost = /^\.rand(om)?/.exec(msg);
     if (s) {
         var min = parseInt(s[2]);
         var max = parseInt(s[3]);
         var math = (min + Math.floor(Math.random() * ((max - min) + 1)))
         dobby.respond(math)
+    } else if (almost){
+        dobby.respond(exports.help[0].join(": "))
     }
 }
