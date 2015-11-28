20614c77767f457884580a38f298466199bcf74c
Quib Daddy <webmaster@quibs.org>
2015-11-27 20:00:48 -0800

    Paintin server plugin locked and loaded

---
+++ plugins/paint.js
@@ -0,0 +1,66 @@
+// neural network painter server
+//
+
+var async = require('async');
+var jot = require('json-over-tcp');
+
+var chanID = '';
+var password = '';
+var ctx = {
+    server: jot.createServer(),
+    painters: []
+};
+
+
+exports.help = [
+    ["paint <http://cuck.com/style1.jpg> <http://cuck.com/stlye2.jpg> http://quibs.info/content.jpg", "Create art."]
+];
+
+exports.config = function(config){
+    ctx.server.listen(config.port);
+    chanID = config.chanID;
+    password = config.password;
+};
+
+exports.init = function(dobby){
+    ctx.server.on('connection', function(socket){
+        var auth = false;
+        socket.on('close', function(){
+            //TODO fix this
+            painters.pop()
+        });
+        socket.on('data', function(data){
+            if (auth == false){
+                if (data.password == password){
+                    auth = true;
+                    painters.push(socket)
+                } else {
+                    socket.end()
+                }
+            } else {
+                dobby.with(function(supervisor){
+                    supervisor.move_to_channel(chanID, function(err){
+                        supervisor.send_channel_message(data.msg, function(){})
+                    })
+                })
+            }
+        })
+    });
+};
+
+exports.onMessage = function(msg, dobby) {
+    var terms = msg.split(" ");
+    var command = terms.shift();
+    //terms = terms.join(" ");
+
+    if ((command == '.paint' || command == '.p') && dobby.cid == chanID) {
+        if (ctx.painters.length != 1){
+            dobby.respond('No painters available.')
+        } else {
+            ctx.painters[0].write({
+                styles: ['https://quibs.org/s/1448682826-75cdda.png'],
+                content: 'https://quibs.org/s/1448682860-cec01b.png'
+            })
+        }
+    }
+};
