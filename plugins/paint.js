9f734ea6a9a979fc7303d66d6bca7c6d0cf4f650
Quib Daddy <webmaster@quibs.org>
2015-12-03 17:33:00 -0800

    Added status channel icons to paint

--- plugins/paint.js
+++ plugins/paint.js
@@ -6,6 +6,9 @@ var jot = require('json-over-tcp');
 
 var chanID = '';
 var password = '';
+var red = '';
+var yellow = '';
+var green = '';
 var ctx = {
     server: jot.createServer(),
     painters: []
@@ -20,27 +23,53 @@ exports.config = function(config){
     ctx.server.listen(config.port);
     chanID = config.chanID;
     password = config.password;
+    red = config.red;
+    yellow = config.yellow;
+    green = config.green;
+
 };
+
+function editIcon(dobby, color){
+    dobby.with(function(supervisor){
+        supervisor.send("channeladdperm", {cid: chanID, permsid: 'i_icon_id', permvalue: color}, function(err){
+            if(err){
+                console.log(err)
+            }
+        })
+    })
+}
 
 exports.init = function(dobby){
     ctx.server.on('connection', function(socket){
         var auth = false;
         socket.on('error', function() {
-            
-        })
+        });
         socket.on('close', function(){
             //TODO fix this
+            editIcon(red);
             ctx.painters.pop()
         });
         socket.on('data', function(data){
             if (auth == false){
                 if (data.password == password){
                     auth = true;
-                    ctx.painters.push(socket)
+                    ctx.painters.push(socket);
+                    editIcon(green);
                 } else {
                     socket.end()
                 }
             } else {
+                switch (data.status){
+                    case 2:
+                        editIcon(green);
+                        break;
+                    case 1:
+                        editIcon(yellow);
+                        break;
+                    default:
+                        editIcon(red);
+                        break;
+                }
                 dobby.with(function(supervisor){
                     supervisor.move_to_channel(chanID, function(err){
                         supervisor.send_channel_message(data.msg, function(){})
