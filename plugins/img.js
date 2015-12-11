a90cb5405f9ab7e53ffbfd2df28d02296d749bfb
Quib Daddy <webmaster@quibs.org>
2015-12-11 15:49:22 -0800

    fixed img plugin

--- plugins/img.js
+++ plugins/img.js
@@ -1,10 +1,9 @@
 // img plugin for Dobby
-
-var unirest = require('unirest');
+var gis = require('g-i-s');
 
 exports.help = [
     [".img <terms>", "Search for an image on Google"]
-]
+];
 
 exports.onMessage = function(msg, dobby) {
     var terms = msg.split(" ");
@@ -12,26 +11,20 @@ exports.onMessage = function(msg, dobby) {
     terms = terms.join(" ");
 
     if (command == '.img') {
-        unirest.get("http://ajax.googleapis.com/ajax/services/search/images")
-            .query({safe: "off", v: "1.0", q: terms})
-            .end(function(response) {
-                if (response.status == 200) {
-                    var body = JSON.parse(response.body);
-                    if (body){
-                        var first = body.responseData.results[0];
-                        if (typeof first != "undefined") {
-                            dobby.respond("[URL]" + first.unescapedUrl + "[/URL]");
-                        } else {
-                            dobby.respond("No results.");
-                        }
-                    } else {
-                        dobby.respond("No response.");
-                    }
+        gis(terms, logResults);
+
+        function logResults(error, results){
+            if (error){
+                dobby.respond('Error: '+ error);
+            } else {
+                if(typeof results[0] == 'undefined'){
+                    dobby.respond('No results!');
                 } else {
-                    dobby.respond("Failed!");
+                    dobby.respond('[url]' + results[0] + '[/url]');
                 }
-            })
+            }
+        }
     }
-}
+};
 
 
