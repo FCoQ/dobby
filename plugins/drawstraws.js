211ffeefdf409677ffe93c5f4cf1ad59e68d59d3
Quib Daddy <webmaster@quibs.org>
2015-12-11 23:55:26 -0800

    Added drawstraws plugin

---
+++ plugins/drawstraws.js
@@ -0,0 +1,23 @@
+// draw straws plugin for dobbeh
+
+exports.help = [
+    [".drawstraws <name1> <name2>", "Picks a random name from the input"]
+];
+
+exports.onMessage = function(msg, dobby) {
+    var terms = msg.split(" ");
+    var command = terms.shift();
+
+    if (command == '.drawstraws') {
+        if (terms.length != -1) {
+            var winner = terms[Math.floor(Math.random() * terms.length)];
+            if (typeof winner == 'undefined') {
+                dobby.respond("[b]Invalid input:[/b] .drawstaws name1 name2 name3")
+            } else {
+                dobby.respond('Winner: ' + '[b]' + winner + '[/b]')
+            }
+        }
+    }
+};
+
+
